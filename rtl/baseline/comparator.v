6146822e314828f2a5dd1e7fb2e9b3604be1ff41
ADITHYAN <adithyana8833@gmail.com>
2026-02-23 18:39:24 +0530

    Update comparator.v

--- rtl/baseline/comparator.v
+++ rtl/baseline/comparator.v
@@ -1,1 +1,13 @@
+module comparator(
+  input [1:0] req_floor,
+  input [1:0] curr_floor,
+  output wire UP,
+  output wire DOWN,
+  output wire EQ
+  );
+  
+  assign UP = (req_floor > curr_floor);
+  assign DOWN = (req_floor < curr_floor);
+  assign EQ = (req_floor == curr_floor);
+endmodule
 
