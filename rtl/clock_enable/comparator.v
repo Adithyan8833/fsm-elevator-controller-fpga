6c15803607148c00c265b45bd33e2bf4c09cf4ad
ADITHYAN <adithyana8833@gmail.com>
2026-02-23 19:11:28 +0530

    Update comparator.v

--- rtl/clock_enable/comparator.v
+++ rtl/clock_enable/comparator.v
@@ -1,1 +1,33 @@
-
+module door_timer(
+  input clk,
+  input reset,
+  input door_open,
+  output reg T
+  );  
+ localparam COUNT_MAX = 100;
+ reg [6:0] count;
+ 
+ always @(posedge clk or posedge reset)
+ begin
+    if (reset) begin
+        count <= 0;
+        T <= 0;
+    end
+    else begin
+        if (door_open) begin
+            if (count == COUNT_MAX) begin
+                count <= 0;
+                T <= 1;
+            end
+            else begin
+                count <= count + 1;
+                T <= 0;
+            end
+        end
+        else begin
+            count <= 0;
+            T <= 0;
+        end
+    end
+ end
+endmodule
